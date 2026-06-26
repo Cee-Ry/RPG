7e733e1786414d46e38416d2eadc653421840562
Cee-Ry <eduaveceasar@gmail.com>
2026-06-26 22:36:25 +0800

    fix: clean close function

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,24 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-	sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "SFML works!" );
-	sf::CircleShape shape( 100.f );
-	shape.setFillColor( sf::Color::Green );
+void close_window(sf::RenderWindow &window);
 
-	while ( window.isOpen() )
-	{
-		while ( const std::optional event = window.pollEvent() )
-		{
-			if ( event->is<sf::Event::Closed>() )
-				window.close();
-		}
+int main() {
+  sf::RenderWindow window;
+  window.create(sf::VideoMode({1366, 768}), "RougeLIke");
 
-		window.clear();
-		window.draw( shape );
-		window.display();
-	}
+  while(window.isOpen()) {
+    close_window(window);
+
+    window.clear(sf::Color::Black);
+    window.display();
+  }
+
+  return 0;
+}
+
+// making this func to make the main loop look cleaner
+void close_window(sf::RenderWindow &window) {
+  while(const std::optional event = window.pollEvent()) 
+    if (event->is<sf::Event::Closed>()) 
+      window.close();
 }
